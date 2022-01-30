4d976be98aff6aa0dfb30bcfbe58d6237cf4ab4c
rianamrodin <62143016+rianamrodin@users.noreply.github.com>
2022-01-30 19:24:00 +0700

    tes index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- head definitions go here -->
+    </head>
+    <body>
+    <p>tes</p>
+    <?php
+    echo "hello";
+    ?>
+        <!-- the content goes here -->
+    </body>
+</html>
